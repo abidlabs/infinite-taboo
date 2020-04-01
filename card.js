12e7e10fcb5b3444a131e3d1f0a04ff6d0c81c90
Abubakar Abid <a12d@stanford.edu>
2020-04-01 11:31:22 -0700

    hooked up data1 with random

--- card.js
+++ card.js
@@ -1,11 +1,38 @@
 var data;
 	$.ajax({
 	  type: "GET",  
-	  url: "out_data/data1.csv",
+	  url: "https://abidlabs.github.io/infinite-taboo/out_data/data1.csv",
 	  dataType: "text",       
 	  success: function(response)  
 	  {
 		data = $.csv.toArrays(response);
-		console.log(data);
+		pickRandomSample(data);
 	  }   
 	});
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function pickRandomSample(fullData){
+	var sample = fullData[Math.floor(Math.random() * fullData.length)];
+	setCardData(sample)
+}
+
+function setCardData(sample){
+	$("#key-word").html(sample[0])
+	
+	var taboo_words = sample[1].split(",")
+	shuffleArray(taboo_words)
+	
+	var taboo_words_holders = $('#taboos-div').children()
+	for (var i = 0; i < taboo_words_holders.length; i++){
+		console.log(taboo_words_holders[i])
+		console.log(taboo_words[i])		
+		taboo_words_holders[i].innerHTML = taboo_words[i]
+	}
+
+}
